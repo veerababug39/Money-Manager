084e5c04a0c641c70e60339f612df6b30e9e0bb0
veerababug39 <veerababug39@gmail.com>
2023-09-11 05:04:16 +0000

    Money Manager Solution

--- src/components/TransactionItem/index.js
+++ src/components/TransactionItem/index.js
@@ -12,7 +12,7 @@ const TransactionItem = props => {
   return (
     <li className="table-row">
       <p className="transaction-text">{title}</p>
-      <p className="transaction-text">{amount}</p>
+      <p className="transaction-text">Rs {amount}</p>
       <p className="transaction-text">{type}</p>
       <din className="delete-container">
         <button
